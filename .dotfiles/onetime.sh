5707e9b1c1f85d8baf64f66546c14c7240d72585
Ben Weston <35574350+bweston6@users.noreply.github.com>
2021-06-06 16:53:02 +0100

    Completing base script.

--- .dotfiles/onetime.sh
+++ .dotfiles/onetime.sh
@@ -13,10 +13,52 @@ useradd -m -G wheel -s /bin/zsh $NAME
 echo "Enter a password for $NAME (you will soon be editing the sudoers file; allow wheel access):"
 passwd $NAME
 visudo
+mkdir -p /home/$NAME/.dotfiles/boot
+chown -R $NAME:$NAME /home/$NAME/.dotfiles
+echo -e "\n/boot\t/home/$NAME/.dotfiles/boot\tnone\tdefaults,bind\t0\t0" >> /etc/fstab
+mount -a
 
 # Configuring Network
-echo "Configuring Network - This will only work with Ethernet"
-
+echo "Configuring Network"
+echo "Ensure that Ethernet is connected and press enter to continue:"
+read
+cp /mnt/tmp/dotfiles/etc/systemd/network/20-wired.network /etc/systemd/network/
+systemctl enable --now systemd-networkd.service systemd-resolved.service
 
 # Installing system-specific programs
+echo "You may need to enter the password for $NAME:"
+sudo -i -u $NAME zsh << EOT
+# Installing oh-myzsh for user
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Installing Yay
+cd ~
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+yay -Syu systemd-boot-pacman-hook vi-vim-symlink
+
+# Installing Dotfiles
+cd ~
+git init
+git remote add origin https://github.com/bweston6/dotfiles.git
+git fetch
+git reset origin/main
+git checkout -t origin/master
+git reset --hard HEAD
+
+# Installing Vim-Plug Plugins
+vim +PlugInstall +PlugUpdate +PlugClean! +PlugUpgrade +qall
+EOT
+
+# Installing oh-my-zsh for Root
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp /mnt/tmp/dotfiles/root/.zshrc /root
+
+# Removing Script
+systemctl disable onetime.service
+rm /etc/systemd/system/onetime.service
+echo "Please reboot to enact all changes."
+rm -- "$0"
