4c63e23c807f12fe3a67e8e8a0ce17b82f590cf4
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-01-12 11:44:00 +0000

    Updating extensions.

--- stow/home/gnome-extensions/.local/share/gnome-shell/extensions/display-brightness-ddcutil@themightydeity.github.com/extension.js
+++ stow/home/gnome-extensions/.local/share/gnome-shell/extensions/display-brightness-ddcutil@themightydeity.github.com/extension.js
@@ -18,28 +18,40 @@
 
 const Main = imports.ui.main;
 const ByteArray = imports.byteArray;
-const {GLib, Gio, St} = imports.gi;
+const { GLib, Gio, Meta, Shell, St } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const Gettext = imports.gettext;
+
+
+// i18n
+/*
+//some Pop_OS users started getting _ is not a function error with the line below
+const _ = ExtensionUtils.gettext;
+//so I had to use the old code instead
+*/
+const Gettext = imports.gettext.domain(Me.metadata['gettext-domain']);
+const _ = Gettext.gettext;
 
 const Convenience = Me.imports.convenience;
 
-const Domain = Gettext.domain(Me.metadata['gettext-domain']);
-const _ = Domain.gettext;
-
 //for ui stuff of this extension
-const { 
-    StatusAreaBrightnessMenu, 
-    SingleMonitorMenuItem, 
-    SingleMonitorSliderAndValue } = Me.imports.ui;
+const {
+    StatusAreaBrightnessMenu,
+    SystemMenuBrightnessMenu,
+    SingleMonitorSliderAndValue
+} = Me.imports.indicator;
 
 const PopupMenu = imports.ui.popupMenu;
+const AggregateMenu = Main.panel.statusArea.aggregateMenu;
 
-const {SHOW_ALL_SLIDER, SHOW_VALUE_LABEL} = Me.imports.convenience;
+const {
+    brightnessLog
+} = Me.imports.convenience;
 
-/* lowest possible value for brightness */
+/* lowest possible value for brightness
+    this is skipped if allow-zero-brightness is set
+*/
 const minBrightness = 1;
 
 /* when should min brightness value should be used */
@@ -48,8 +60,6 @@ const minBrightnessThreshold = 5;
 let displays = [];
 
 let mainMenuButton = null;
-
-let timeoutId = null;
 
 /*
     instead of reading i2c bus everytime during startup,
@@ -63,7 +73,6 @@ const ddcutil_detect_cache_file = `${cache_dir}/ddcutil_detect`;
 
 const ddcutil_path = "/usr/bin/ddcutil";
 
-
 class DDCUtilBrightnessControlExtension {
     constructor() { }
     enable() {
@@ -75,116 +84,183 @@ class DDCUtilBrightnessControlExtension {
 }
 
 function init() {
-    ExtensionUtils.initTranslations(Me.metadata['gettext-domain']);
+    ExtensionUtils.initTranslations();
 
     return new DDCUtilBrightnessControlExtension();
 }
 
-function brightnessLog(str){
-    log("display-brightness-ddcutil extension:\n"+str);
-}
+
 function BrightnessControl(set) {
+    let settings = ExtensionUtils.getSettings();
     if (set == "enable") {
-        mainMenuButton = new StatusAreaBrightnessMenu();
-        Main.panel.addToStatusArea("DDCUtilBrightnessSlider", mainMenuButton, 0, "right");
+        displays = [];
+        if (settings.get_string('button-location') == "panel") {
+            brightnessLog("Adding to panel");
+            mainMenuButton = new StatusAreaBrightnessMenu();
+            Main.panel.addToStatusArea("DDCUtilBrightnessSlider", mainMenuButton, 0, "right");
+        } else {
+            brightnessLog("Adding to system menu");
+            mainMenuButton = new SystemMenuBrightnessMenu(settings);
+            AggregateMenu._indicators.add_child(mainMenuButton);
+            AggregateMenu.menu.addMenuItem(mainMenuButton.menu, settings.get_double('position-system-menu'));
+        }
         if (mainMenuButton !== null) {
             /* connect all signals */
-            connectSettingsSignals();
+            connectSettingsSignals(settings);
             connectMonitorChangeSignals();
 
-            addTextItemToPanel(_("Initializing"));
-            addSettingsItem();
+            addKeyboardShortcuts(settings);
 
-            addAllDisplaysToPanel();
+            if (settings.get_string('button-location') == "panel") {
+                addTextItemToPanel(_("Initializing"));
+                addSettingsItem();
+            }
+
+            addAllDisplaysToPanel(settings);
         }
-        
 
     } else if (set == "disable") {
         /* disconnect all signals */
-        disconnectSettingsSignals();
+        disconnectSettingsSignals(settings);
         disconnectMonitorSignals();
 
+        /* remove shortcuts */
+        removeKeyboardShortcuts();
+
+        /* clear timeouts */
+        if (_reloadMenuWidgetsTimer) {
+            Convenience.clearTimeout(_reloadMenuWidgetsTimer);
+        }
+        displays.forEach(display=>{
+            if('slider' in display){
+                display.slider.destory();
+            }
+        })
+
+        /* clear variables */
         mainMenuButton.destroy();
         mainMenuButton = null;
-        displays = [];
-        if (timeoutId) {
-            Convenience.clearTimeout(timeoutId)
-        }
+        displays = null;
+        settings = null;
     }
 }
 
-function setBrightness(display, newValue) {
+function setBrightness(settings, display, newValue) {
     let newBrightness = parseInt((newValue / 100) * display.max);
     if (newBrightness <= minBrightnessThreshold) {
-        newBrightness = minBrightness;
+        if (settings.get_boolean('allow-zero-brightness')) {
+            newBrightness = 0;
+        } else {
+            newBrightness = minBrightness;
+        }
     }
+    //brightnessLog(`${ddcutil_path} setvcp 10 ${newBrightness} --bus ${display.bus}`)
     GLib.spawn_command_line_async(`${ddcutil_path} setvcp 10 ${newBrightness} --bus ${display.bus}`)
 }
 
-function setAllBrightness(newValue) {
-    displays.forEach(element => {
-        element.slider.changeValue(newValue);
-        setBrightness(element, newValue);
+function setAllBrightness(settings, newValue) {
+    displays.forEach(display => {
+        display.slider.changeValue(newValue);
+        setBrightness(settings, display, newValue);
     });
 }
 
 function addSettingsItem() {
     let settingsItem = new PopupMenu.PopupMenuItem(_("Settings"));
-    settingsItem.connect('activate', openPrefs);
-    mainMenuButton.addMenuItem(settingsItem,1);
+    settingsItem.connect('activate', ()=>{
+        ExtensionUtils.openPrefs();
+    });
+    mainMenuButton.addMenuItem(settingsItem, 1);
 
     let reloadItem = new PopupMenu.PopupMenuItem(_("Reload"));
     reloadItem.connect('activate', event => {
-        BrightnessControl("disable");
-        BrightnessControl("enable");
+        reloadExtension();
     });
-    mainMenuButton.addMenuItem(reloadItem,2);
+    mainMenuButton.addMenuItem(reloadItem, 2);
 }
 
-function addAllSlider() {
+function addAllSlider(settings) {
     let onAllSliderChange = function (newValue) {
-        setAllBrightness(newValue);
+        setAllBrightness(settings, newValue);
     }
-    let allslider = new SingleMonitorSliderAndValue(_("All"), displays[0].current, onAllSliderChange);
+    let allslider = new SingleMonitorSliderAndValue(settings, _("All"), displays[0].current, onAllSliderChange);
     mainMenuButton.addMenuItem(allslider)
+
+    /* save slider in main menu, so that it can be accessed easily for different events */
+    mainMenuButton.storeValueSliderForEvents(allslider.getValueSlider())
 }
 
-function addDisplayToPanel(display) {
+function addDisplayToPanel(settings, display) {
     let onSliderChange = function (newValue) {
-        setBrightness(display, newValue)
+        setBrightness(settings, display, newValue)
     }
-    let displaySlider = new SingleMonitorSliderAndValue(display.name, display.current, onSliderChange);
+    let displaySlider = new SingleMonitorSliderAndValue(settings, display.name, display.current, onSliderChange);
     display.slider = displaySlider;
     mainMenuButton.addMenuItem(displaySlider);
+
+    /* when "All" slider is shown we do not need to store each display's value slider */
+    /* save slider in main menu, so that it can be accessed easily for different events */
+    if (!settings.get_boolean('show-all-slider')) {
+        mainMenuButton.storeValueSliderForEvents(displaySlider.getValueSlider())
+    }
+
 }
 
-function reloadMenuWidgets() {
-    if(mainMenuButton === null){
+let _reloadMenuWidgetsTimer = null;
+/* 
+   reload menu widgets being called many time caused some lag
+   after every display info was parsed, add this should run reloadMenuWidgets only once
+ */
+function reloadMenuWidgets(settings) {
+    if (_reloadMenuWidgetsTimer) {
+        Convenience.clearTimeout(_reloadMenuWidgetsTimer);
+    }
+    _reloadMenuWidgetsTimer = Convenience.setTimeout(() => {
+        _reloadMenuWidgetsTimer = null;
+        _reloadMenuWidgets(settings);
+    }, 1000)
+}
+
+function _reloadMenuWidgets(settings) {
+    if (mainMenuButton === null) {
         return;
     }
-    mainMenuButton.removeAllMenu();
 
-    if (settings.get_boolean(SHOW_ALL_SLIDER)) {
-        addAllSlider();
+    brightnessLog("Reloading widgets");
+
+    mainMenuButton.removeAllMenu();
+    mainMenuButton.clearStoredValueSliders();
+
+    if (settings.get_boolean('show-all-slider')) {
+        addAllSlider(settings);
     }
     displays.forEach(display => {
-        addDisplayToPanel(display);
+        addDisplayToPanel(settings, display);
     });
-    addSettingsItem();
+
+    if (settings.get_string('button-location') == "panel") {
+        addSettingsItem();
+    }
+}
+
+function reloadExtension() {
+    brightnessLog("Reload extension");
+    BrightnessControl("disable");
+    BrightnessControl("enable");
 }
 
 function addTextItemToPanel(text) {
-    if(mainMenuButton === null) return;
+    if (mainMenuButton === null) return;
     let menuItem = new PopupMenu.PopupMenuItem(text, {
         reactive: false
     });
     mainMenuButton.addMenuItem(menuItem);
 }
 
-function parseDisplaysInfoAndAddToPanel(ddcutil_brief_info, panel) {
+function parseDisplaysInfoAndAddToPanel(settings, ddcutil_brief_info) {
     try {
         let display_names = [];
-        /* 
+        /*
         due to spawnWithCallback fetching faster information for second display in list before first one
         there is a situation where name is displayed for first device but controls second device.
 
@@ -194,23 +270,28 @@ function parseDisplaysInfoAndAddToPanel(ddcutil_brief_info, panel) {
         brightnessLog("ddcutil brief info:\n" + ddcutil_brief_info);
         ddcutil_brief_info.split('\n').map(ddc_line => {
             if (ddc_line.indexOf("/dev/i2c-") !== -1) {
-                brightnessLog("ddcutil brief info found bus line:\n" +  " "+ ddc_line)
+                brightnessLog("ddcutil brief info found bus line:\n" + " " + ddc_line)
                 /* I2C bus comes first, so when that is detect start a new display object */
                 let display_bus = ddc_line.split("/dev/i2c-")[1].trim();
                 /* save diplay_loop_id as a const for rest of the async calls below here*/
                 const display_id = diplay_loop_id;
                 /* check if display is on or not */
-                brightnessLog("ddcutil reading display state for bus: " +display_bus)
+                brightnessLog("ddcutil reading display state for bus: " + display_bus)
                 Convenience.spawnWithCallback([ddcutil_path, "getvcp", "--brief", "D6", "--bus", display_bus], function (vcpPowerInfos) {
-                    brightnessLog("ddcutil reading display status for bus: " + display_bus +" is: " + vcpPowerInfos)
+                    brightnessLog("ddcutil reading display status for bus: " + display_bus + " is: " + vcpPowerInfos)
                     /* only add display to list if ddc communication is supported with the bus*/
                     if (vcpPowerInfos.indexOf("DDC communication failed") === -1) {
                         let vcpPowerInfosArray = vcpPowerInfos.trim().split(" ");
-                        /* 
-                         D6 = Power mode
-                         x01 = DPM: On,  DPMS: Off
-                        */
-                        if (vcpPowerInfosArray.length >= 4  && vcpPowerInfosArray[3] == "x01"){
+
+                        let stateCheck = (vcpPowerInfosArray.length >= 4);
+                        if (!settings.get_boolean('disable-display-state-check')) {
+                            /*
+                             D6 = Power mode
+                             x01 = DPM: On,  DPMS: Off
+                            */
+                            stateCheck = (stateCheck && vcpPowerInfosArray[3] == "x01")
+                        }
+                        if (stateCheck) {
                             /* read the current and max brightness using getvcp 10 */
                             Convenience.spawnWithCallback([ddcutil_path, "getvcp", "--brief", "10", "--bus", display_bus], function (vcpInfos) {
                                 let display = {};
@@ -221,21 +302,21 @@ function parseDisplaysInfoAndAddToPanel(ddcutil_brief_info, panel) {
                                 let currentBrightness = vcpInfosArray[3] / vcpInfosArray[4];
 
                                 /* make display object */
-                                display = { "bus": display_bus, "max": maxBrightness, "current": currentBrightness, "name": display_names[display_id]};
+                                display = { "bus": display_bus, "max": maxBrightness, "current": currentBrightness, "name": display_names[display_id] };
                                 displays.push(display);
 
                                 /* cheap way of making reloading all display slider in the panel */
-                                reloadMenuWidgets(panel);
+                                reloadMenuWidgets(settings);
                             });
                         }
                     }
                 });
-                
+
             }
             if (ddc_line.indexOf("Monitor:") !== -1) {
                 /* Monitor name comes second in the output,
                  so when that is detected fill the object and push it to list */
-                display_names[diplay_loop_id] = ddc_line.split(_("Monitor:"))[1].trim().split(":")[1].trim()
+                display_names[diplay_loop_id] = ddc_line.split("Monitor:")[1].trim().split(":")[1].trim()
                 diplay_loop_id++;
             }
         });
@@ -244,19 +325,19 @@ function parseDisplaysInfoAndAddToPanel(ddcutil_brief_info, panel) {
     }
 }
 
-function getDisplaysInfoAsync(panel) {
+function getDisplaysInfoAsync(settings) {
     Convenience.spawnWithCallback([ddcutil_path, "detect", "--brief"], function (stdout) {
-        parseDisplaysInfoAndAddToPanel(stdout, panel);
+        parseDisplaysInfoAndAddToPanel(settings, stdout);
     });
 }
 
-function getCachedDisplayInfoAsync(panel) {
+function getCachedDisplayInfoAsync(settings) {
     let file = Gio.File.new_for_path(ddcutil_detect_cache_file)
     let cancellable = new Gio.Cancellable();
     file.load_contents_async(cancellable, (source, result) => {
         try {
             let [ok, contents, etag_out] = source.load_contents_finish(result);
-            parseDisplaysInfoAndAddToPanel(ByteArray.toString(contents), panel);
+            parseDisplaysInfoAndAddToPanel(settings, ByteArray.toString(contents));
         } catch (e) {
             brightnessLog(`${ddcutil_detect_cache_file} cache file reading error`)
         }
@@ -264,42 +345,46 @@ function getCachedDisplayInfoAsync(panel) {
     Convenience.spawnWithCallback(["cat", ddcutil_detect_cache_file], function (stdout) { });
 }
 
-
-
-function onSettingsChange(){
+function onSettingsChange(settings) {
     brightnessLog("Settings change detected, reloading widgets")
-    reloadMenuWidgets()
+    removeKeyboardShortcuts()
+    addKeyboardShortcuts(settings)
+    reloadMenuWidgets(settings)
 }
 
 let monitorChangeTimeout = null;
 
-function onMonitorChange(){
+function onMonitorChange() {
     /* 
-    when monitor change happens, 
+    when monitor change happens,
     sometimes the turned off monitor is still accepting DDC connection
-    this is not a great fix, because some monitor 
+    this is not a great fix, because some monitor
     will still take longer than 5 seconds to be off
     */
-   brightnessLog("Monitor change detected, reloading extension in 5 seconds.")
-    if(monitorChangeTimeout !== null){
+    brightnessLog("Monitor change detected, reloading extension in 5 seconds.")
+    if (monitorChangeTimeout !== null) {
         Convenience.clearTimeout(monitorChangeTimeout)
     }
-    monitorChangeTimeout = Convenience.setTimeout(function(){
+    monitorChangeTimeout = Convenience.setTimeout(function () {
         monitorChangeTimeout = null;
-        BrightnessControl("disable");
-        BrightnessControl("enable");
+        reloadExtension();
     }, 5000);
 
 }
 
 let settingsSignals = {};
 
-function connectSettingsSignals() {
+function connectSettingsSignals(settings) {
     settingsSignals = {
-        change: settings.connect('changed', onSettingsChange)
+        change: settings.connect('changed', ()=>{
+            onSettingsChange(settings)
+        }),
+        reload: settings.connect('changed::reload', reloadExtension),
+        indicator: settings.connect('changed::button-location', reloadExtension),
+        hide_system_indicator: settings.connect('changed::hide-system-indicator', reloadExtension),
+        position_system_menu: settings.connect('changed::position-system-menu', reloadExtension)
     }
 }
-
 
 let monitorSignals = {}
 
@@ -309,7 +394,7 @@ function connectMonitorChangeSignals() {
     }
 }
 
-function disconnectSettingsSignals() {
+function disconnectSettingsSignals(settings) {
     settings.disconnect(settingsSignals.change);
 }
 
@@ -317,28 +402,48 @@ function disconnectMonitorSignals() {
     Main.layoutManager.disconnect(monitorSignals.change);
 }
 
-function addAllDisplaysToPanel(){
+function addAllDisplaysToPanel(settings) {
     try {
         if (GLib.file_test(ddcutil_detect_cache_file, (GLib.FileTest.IS_REGULAR))) {
-            getCachedDisplayInfoAsync(mainMenuButton);
+            getCachedDisplayInfoAsync(settings);
         } else {
-            getDisplaysInfoAsync(mainMenuButton);
+            getDisplaysInfoAsync(settings);
         }
     } catch (err) {
         brightnessLog(err);
     }
 }
 
+function increase() {
+    brightnessLog("Increase brightness");
+    mainMenuButton.emit('value-up')
+}
 
-function openPrefs() {
-    if (typeof ExtensionUtils.openPrefs === 'function') {
-        ExtensionUtils.openPrefs();
-    } else {
-        Util.spawn(['sh', '-c',
-            'command -v gnome-extensions 2>&1 && gnome-extensions prefs ' +
-            Me.uuid +
-            ' || gnome-shell-extension-prefs ' +
-            Me.uuid
-        ]);
-    }
+function decrease() {
+    brightnessLog("Decrease brightness");
+    mainMenuButton.emit('value-down');
+}
+
+function addKeyboardShortcuts(settings) {
+    brightnessLog("Add keyboard shortcuts");
+    Main.wm.addKeybinding(
+        'increase-brightness-shortcut',
+        settings,
+        Meta.KeyBindingFlags.NONE,
+        Shell.ActionMode.ALL,
+        this.increase.bind(this)
+    );
+    Main.wm.addKeybinding(
+        'decrease-brightness-shortcut',
+        settings,
+        Meta.KeyBindingFlags.NONE,
+        Shell.ActionMode.ALL,
+        this.decrease.bind(this)
+    );
+}
+
+function removeKeyboardShortcuts() {
+    brightnessLog("Remove keyboard shortcuts");
+    Main.wm.removeKeybinding('increase-brightness-shortcut');
+    Main.wm.removeKeybinding('decrease-brightness-shortcut');
 }
