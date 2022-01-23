be80ad4950989c73191d69d0befffe61670d5467
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-01-23 20:59:12 +0000

    Fixing command test and adding inetutils.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 # setting variables
-PWD=$(dirname $(readlink -f $0))
 DISTRIBUTION=$(lsb_release -i | cut -f 2)
 
 # installing pre-requisites
 if [ "$DISTRIBUTION" = 'Arch' ];
 then
-	if ! command -v ansible >/dev/null 2>&1;
+	if ! command -v 'ansible' >/dev/null 2>&1;
 	then
-		sudo pacman -S --needed ansible
+		sudo pacman -S --needed --noconfirm ansible inetutils
+	fi
+	if ! command -v 'hostname' >/dev/null 2>&1;
+	then
+		sudo pacman -S --needed --noconfirm inetutils
 	fi
 	# running playbook
+	PWD=$(dirname $(readlink -f $0))
 	if [ -f ~/.ssh/id_ed25519 ];
 	then
 		ansible-playbook $PWD/local.yml -i $PWD/hosts -l $(hostname -f)
@@ -24,6 +28,7 @@ then
 		sudo apt install ansible
 	fi
 	# running playbook
+	PWD=$(dirname $(readlink -f $0))
 	if [ -f ~/.ssh/id_ed25519 ];
 	then
 		ansible-playbook $PWD/local.yml -i $PWD/hosts -l $(hostname -f)
